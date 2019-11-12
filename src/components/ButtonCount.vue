c7e493359b7e5edfa3f3e285f4c0a14d820dfb9b
fernandacancino <fernand4.reyes@gmail.com>
2019-11-12 13:11:19 -0300

    first commit, testing a project in vue with jest and travis

---
+++ src/components/ButtonCount.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h3>Welcome</h3>
+
+        <p>Button count is: {{count}}</p>
+        <button @click="changeCount"> Click </button>
+
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    changeCount () {
+      this.count += 1
+    }
+  }
+}
+</script>
